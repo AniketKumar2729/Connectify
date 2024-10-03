8fba294dacd2654ee396e8fcdbe6a9d9a5955f53
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-10-03 15:11:42 +0530

    Serveral folder are made under src and routing is made in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React, { lazy } from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+const Home= lazy(()=>import("./pages/Home.jsx"));
+const Login=lazy(()=>import("./pages/Login.jsx"));
+const Chat=lazy(()=>import("./pages/Chat.jsx"));
+const Group=lazy(()=>import("./pages/Group.jsx"));
 function App() {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/chat' element={<Chat/>}/>
+          <Route path='/group' element={<Group/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
