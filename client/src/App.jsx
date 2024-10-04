7553856fd18167b2ec53fde910e7bc96d24539f0
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-10-04 22:28:55 +0530

    NotFound page route added and how to protect route is coded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,25 @@
 import React, { lazy } from 'react'
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-const Home= lazy(()=>import("./pages/Home.jsx"));
-const Login=lazy(()=>import("./pages/Login.jsx"));
-const Chat=lazy(()=>import("./pages/Chat.jsx"));
-const Group=lazy(()=>import("./pages/Group.jsx"));
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from './components/auth/ProtectedRoute.jsx';
+const Home = lazy(() => import("./pages/Home.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const Chat = lazy(() => import("./pages/Chat.jsx"));
+const Group = lazy(() => import("./pages/Group.jsx"));
+const NotFound=lazy(()=> import("./pages/NotFound.jsx"))
+let user = false;
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/chat' element={<Chat/>}/>
-          <Route path='/group' element={<Group/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route element={<ProtectedRoute user={user} />}>
+
+            <Route path='/' element={<Home />} />
+            <Route path='/chat' element={<Chat />} />
+            <Route path='/group' element={<Group />} />
+          </Route>
+          <Route path='/login' element={<ProtectedRoute user={!user} redirect='/'><Login/></ProtectedRoute>} />
+          <Route path="*" element={<NotFound />}/>
         </Routes>
       </BrowserRouter>
     </>
