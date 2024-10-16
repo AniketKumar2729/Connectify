70f01db3087716e61068e638c80681ab8c578762
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-10-16 16:42:08 +0530

    Notification shade created

--- client/src/components/specific/Notification.jsx
+++ client/src/components/specific/Notification.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
-
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogTitle,
+  ListItem,
+  Stack,
+  Typography
+} from "@mui/material";
+import React, { memo } from "react";
+import { sampleNotificaiton } from "../../constants/sampleData.js";
 const Notification = () => {
+  const friendRequestHandler = ({ _id, accept }) => {};
   return (
-    <div>Notification</div>
-  )
-}
+    <Dialog open>
+      <Stack p={{ xs: "1rem", sm: "2rem" }} maxWidth={"25rem"}>
+        <DialogTitle>Notification</DialogTitle>
+        {sampleNotificaiton.length > 0 ? (
+          sampleNotificaiton.map((i) => (
+            <NotificationItem
+              sender={i.sender}
+              _id={i._id}
+              handler={friendRequestHandler}
+              key={i._id}
+            />
+          ))
+        ) : (
+          <Typography textAlign={"center"}>0 Notifications</Typography>
+        )}
+      </Stack>
+    </Dialog>
+  );
+};
 
-export default Notification
+const NotificationItem = memo(({ sender, _id, handler }) => {
+  const { name, avatar } = sender;
+  return (
+    <ListItem>
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        spacing={"1rem"}
+        width={"100%"}
+      >
+        <Avatar src={avatar[0]} />
+        <Typography
+          variant="body1"
+          sx={{
+            flexGrow: 1,
+            display: "-webkit-box",
+            WebkitLineClamp: 1,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {`${name} sent you a friend requreset`}
+        </Typography>
+        <Stack direction={{xs:'column',sm:'row'}}>
+          <Button onClick={()=>handler({_id,accept:true})}>Accept</Button>
+          <Button color="error" onClick={()=>handler({_id,accept:false})}>Reject</Button>
+        </Stack>
+      </Stack>
+    </ListItem>
+  );
+});
+export default Notification;
--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -1,7 +1,7 @@
 
 export const sampleChats = [
     {
-        avatar: ["https://avatar.iran.liara.run/public/boy?username=Ash"],
+        avatar: ["https://i.pinimg.com/originals/bb/dc/18/bbdc18644f48cfc1d0ac47a17bba2f16.jpg"],
         name: "John Wick",
         _id: "1",
         groupChat: false,
@@ -26,7 +26,7 @@ export const sampleUser = [
     {
         name: "John Wick",
         _id: "1",
-        avatar: ["https://avatar.iran.liara.run/public/boy?username=Ash"],
+        avatar: ["https://i.pinimg.com/originals/bb/dc/18/bbdc18644f48cfc1d0ac47a17bba2f16.jpg"],
     }, {
         name: "Boogeyman",
         _id: "2",
@@ -35,5 +35,28 @@ export const sampleUser = [
         name: "Hitman",
         _id: "3",
         avatar: ["https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png", "https://www.creativefabrica.com/wp-content/uploads/2022/03/09/Woman-Icon-Teen-Profile-Graphics-26722130-1.jpg"],
+    }
+]
+export const sampleNotificaiton = [
+    {
+        _id: "1",
+        sender: {
+            name: "John Wick",
+            avatar: ["https://i.pinimg.com/originals/bb/dc/18/bbdc18644f48cfc1d0ac47a17bba2f16.jpg"],
+        }
+    },
+    {
+        _id: "2",
+        sender: {
+            name: "Boogeyman",
+            avatar: ["https://avatar.iran.liara.run/public/boy?username=Ash"],
+        }
+    },
+    {
+        _id: "3",
+        sender: {
+            name: "Hitman",
+            avatar: ["https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9eb92786-5e60-46ac-ae2c-d33af137f691/da4slp8-57842cc4-a13c-42c9-b54f-b5741c9edd2d.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzllYjkyNzg2LTVlNjAtNDZhYy1hZTJjLWQzM2FmMTM3ZjY5MVwvZGE0c2xwOC01Nzg0MmNjNC1hMTNjLTQyYzktYjU0Zi1iNTc0MWM5ZWRkMmQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.tAPLDlBH-6xpy21jbJ6IeSqfS9-PiXzIaeWylb1rN48"],
+        }
     }
 ]
