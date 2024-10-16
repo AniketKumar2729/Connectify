3dffd9553316afdb45bd9ac0381f1d317296ca50
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-10-16 15:31:30 +0530

    Search Dialog Box created

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {Dialog, DialogTitle, InputAdornment, List, Stack, TextField} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import {useInputValidation} from "6pp";
 
 const Search = () => {
+  const search=useInputValidation("")
   return (
-    <div>Search</div>
+    <Dialog open>
+      <Stack p={'2rem'} direction={'column'}alignItems={'center'} width={'25rem'}>
+      <DialogTitle textAlign={'center'}>Find People</DialogTitle>
+      <TextField label="" value={search.value} onChange={search.changeHandler} variant='outlined' size='small' slotProps={{input:{
+        startAdornment:(
+          <InputAdornment position="start">
+          <SearchIcon/>
+        </InputAdornment>
+        )
+      }}}
+      />
+      
+      </Stack>
+    </Dialog>
   )
 }
 
