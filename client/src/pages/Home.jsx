43999c546ebfb59d894468b2f54c1f81ac76f9c5
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-10-17 22:47:46 +0530

    Simple Home Page created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import AppLayout from '../components/layout/AppLayout'
+import { Box, Typography } from '@mui/material';
 
 function Home() {
-  return (
-    <div>Home</div>
+  return (<>
+  <Box sx={{display:'flex',justifyContent:'center',alignItems:'center'}} height={'100%'} bgcolor={"rgba(0,0,0,0.1)"}>
+
+    <Typography p={'2rem'} variant='h5' textAlign={'center'}   fontSize={'4rem'}> Select an Friend to Chat</Typography>
+  </Box>
+  </>
   )
 }
 
