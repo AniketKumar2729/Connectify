2fab3ceb0c82732d41f9eade80d9e58024f44ee4
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-10-17 23:21:09 +0530

    Simple home Page created and static chatpage created(chat page ui)

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import AppLayout from '../components/layout/AppLayout'
+import AttachmentIcon from '@mui/icons-material/Attachment';
+import SendIcon from '@mui/icons-material/Send';
+import { IconButton, Stack } from '@mui/material';
+import React, { useRef } from 'react';
+import AppLayout from '../components/layout/AppLayout';
+import { InputBox } from '../components/styles/StyledComponent';
+import { gray } from '../constants/color';
 
 function Chat() {
+  const containerRef=useRef(null)
   return (
-    <div>Chat</div>
+    <>
+
+    <Stack ref={containerRef} boxSizing={'border-box'} padding={'1rem'} spacing={'1rem'} bgcolor={gray} height={'90%'} sx={{overflowY:'auto',overflowX:'hidden'}}>
+    {/* messages  render */}
+    </Stack>
+    <form style={{height:"10%"}}>
+      <Stack direction={'row'} height={'100%'} padding={'1rem'} alignItems={'center'} position={'relative'}>
+        <IconButton sx={{position:'absolute',left:'1.5rem',rotate:'-20deg'}}>
+          <AttachmentIcon/>
+        </IconButton>
+        <InputBox placeholder='Enter your message'/>
+        <IconButton type='submit' sx={{rotate:"-20deg",bgcolor:"#72BF78",color:'white',marginLeft:'1rem',padding:'0.5rem',"&:hover":{bgcolor:"#A0D683"}}}>
+        <SendIcon/>
+        </IconButton>
+      </Stack>
+    </form>
+    </>
   )
 }
 
