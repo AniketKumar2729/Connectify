ce3a1eed6ed5c4cb261e355780a2b492f0c89fc3
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-10-24 09:56:20 +0530

    Chat-Management page created, in which table is present with some dummy data

--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -179,84 +179,110 @@ export const sampleMessage = [
     }
 ]
 
-export const dashboardData={
-    users:[
+export const dashboardData = {
+    users: [
         {
             name: "John Wick",
             _id: "1",
             avatar: ["https://i.pinimg.com/originals/bb/dc/18/bbdc18644f48cfc1d0ac47a17bba2f16.jpg"],
-            usename:'John Wick',
-            friends:20,
-            groups:2
+            usename: 'John Wick',
+            friends: 20,
+            groups: 2
         }, {
             name: "Boogeyman",
             _id: "2",
             avatar: ["https://avatar.iran.liara.run/public/boy?username=Ash"],
-            usename:'Boogeyman',
-            friends:21,
-            groups:6
+            usename: 'Boogeyman',
+            friends: 21,
+            groups: 6
         },
         {
             name: "Hitman",
             _id: "3",
             avatar: ["https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png", "https://www.creativefabrica.com/wp-content/uploads/2022/03/09/Woman-Icon-Teen-Profile-Graphics-26722130-1.jpg"],
-            usename:'Hitman',
-            friends:10,
-            groups:0
+            usename: 'Hitman',
+            friends: 10,
+            groups: 0
         },
         {
             name: "Tony Stark",
             _id: "4",
             avatar: ["https://i.abcnewsfe.com/a/fd751005-a167-478b-bba2-ccaf61e26bf6/iron-man-rdjr-1-ht-thg-231204_1701712078177_hpMain.jpg"],
-            usename:'Tony Stark',
-            friends:20,
-            groups:5
+            usename: 'Tony Stark',
+            friends: 20,
+            groups: 5
         },
         {
             name: "Steve Rogers",
             _id: "5",
             avatar: ["https://www.denofgeek.com/wp-content/uploads/2021/01/webstory-captain-america-cover03.jpg"],
-            usename:'Steve Rogers',
-            friends:15,
-            groups:1
+            usename: 'Steve Rogers',
+            friends: 15,
+            groups: 1
         },
         {
             name: "Black Panther",
             _id: "6",
             avatar: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRusBtNv5r6n0fwXG5o867OyEUXfA2zJcGazQ&s"],
-            usename:'Black Panther',
-            friends:50,
-            groups:10
+            usename: 'Black Panther',
+            friends: 50,
+            groups: 10
         },
         {
             name: "Harry Potter",
             _id: "7",
             avatar: ["https://ocdn.apart.eu/apart/apart-eu/jewellery/harry-potter/2024/20240903/harry.jpg"],
-            usename:'Harry Potter',
-            friends:30,
-            groups:15
+            usename: 'Harry Potter',
+            friends: 30,
+            groups: 15
         },
         {
             name: "James Bond",
             _id: "8",
             avatar: ["https://images.immediate.co.uk/production/volatile/sites/3/2023/10/daniel-craig-007.jpg-303a730-a0a1400.png"],
-            usename:'James Bond',
-            friends:12,
-            groups:12
+            usename: 'James Bond',
+            friends: 12,
+            groups: 12
         }, {
             name: "Thor",
             _id: "9",
             avatar: ["https://cdn.britannica.com/73/182873-050-E1C686F4/Chris-Hemsworth-Thor-Thor-The-Dark-World.jpg"],
-            usename:'Thor',
-            friends:44,
-            groups:11
+            usename: 'Thor',
+            friends: 44,
+            groups: 11
         }, {
             name: "Jack Sparrow",
             _id: "10",
             avatar: ["https://i.pinimg.com/474x/fc/d6/e3/fcd6e3afdc58fc8235f177aa9dc93e0d.jpg"],
-            usename:'Jack Sparrow',
-            friends:11,
-            groups:22
+            usename: 'Jack Sparrow',
+            friends: 11,
+            groups: 22
         }
-    ]
+    ],
+    chats: [
+        {
+            _id: "1",
+            avatar: ["https://img.freepik.com/premium-photo/group-boys-with-one-wearing-shirt-that-says-boys_806553-894.jpg?semt=ais_hybrid"],
+            name: "Group-1",
+            totalMember: 2,
+            members: [{_id:"1",avatar:'https://i.abcnewsfe.com/a/fd751005-a167-478b-bba2-ccaf61e26bf6/iron-man-rdjr-1-ht-thg-231204_1701712078177_hpMain.jpg'},{_id:"2",avatar:'https://i.pinimg.com/474x/fc/d6/e3/fcd6e3afdc58fc8235f177aa9dc93e0d.jpg'}],
+            totalMessages: 20,
+            creator: {
+                name: 'John Wick',
+                avatar: "https://i.pinimg.com/originals/bb/dc/18/bbdc18644f48cfc1d0ac47a17bba2f16.jpg"
+            }
+        }, {
+            _id: "2",
+            avatar: ["https://img.freepik.com/free-vector/group-happy-smiling-people-looking-up-top-view-white-background-flat-vector-illustration_1284-78599.jpg"],
+            name: "Group-2",
+            totalMember: 4,
+            members: [{_id:'1',avatar:'https://cdn.britannica.com/73/182873-050-E1C686F4/Chris-Hemsworth-Thor-Thor-The-Dark-World.jpg'},{_id:'2',avatar:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRusBtNv5r6n0fwXG5o867OyEUXfA2zJcGazQ&s'},{_id:'3',avatar:"https://images.immediate.co.uk/production/volatile/sites/3/2023/10/daniel-craig-007.jpg-303a730-a0a1400.png"},{_id:'4',avatar:'https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png", "https://www.creativefabrica.com/wp-content/uploads/2022/03/09/Woman-Icon-Teen-Profile-Graphics-26722130-1.jpg'}],
+            totalMessages: 40,
+            creator: {
+                name: 'Boogeyman',
+                avatar: "https://avatar.iran.liara.run/public/boy?username=Ash"
+            }
+        },
+    ],
+
 }
--- client/src/pages/admin/ChatManagement.jsx
+++ client/src/pages/admin/ChatManagement.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminLayout from '../../components/layout/AdminLayout'
-
+import Table from '../../components/shared/Table'
+import AvatarCard from "../../components/shared/AvatarCard.jsx";
+import { Avatar, Stack } from '@mui/material'
+import { dashboardData } from '../../constants/sampleData'
+import { tranformImage } from '../../lib/features'
+const columns = [
+  {
+    field: 'id', headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: 'avatar', headerName: "Avatar",
+    headerClassName: "table-header",
+    width: 150,
+    renderCell: (params) => <AvatarCard avatar={params.row.avatar} />
+  },
+  {
+    field: 'name', headerName: "Name",
+    headerClassName: "table-header",
+    width: 300,
+  },
+  {
+    field: 'totalMember', headerName: "Total Member",
+    headerClassName: "table-header",
+    width: 120,
+  },
+  {
+    field: 'members', headerName: "Members",
+    headerClassName: "table-header",
+    width: 400,
+    renderCell: (params) => <AvatarCard max={100} avatar={params.row.members} />
+  },
+  {
+    field: 'totalMessages', headerName: "Total Messages",
+    headerClassName: "table-header",
+    width: 150,
+  },
+  {
+    field: 'creator', headerName: "Created By",
+    headerClassName: "table-header",
+    width: 250,
+    renderCell: (params) => (
+      <Stack direction={'row'} spacing={'1rem'} alignItems={'center'}>
+        <Avatar alt={params.row.creator.name} src={params.row.creator.avatar} />
+        <span>{params.row.creator.name}</span>
+      </Stack>
+    )
+  },
+]
 const ChatManagement = () => {
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    setRows(dashboardData.chats.map((i)=>({
+      ...i,
+      id:i._id,
+      totalMember:i.totalMember,
+      totalMessages:i.totalMessages,
+      avatar:i.avatar.map(i=>tranformImage(i,50)),
+      members:i.members.map((i)=>tranformImage(i.avatar,50)),
+      creator:{
+        name:i.creator.name,
+        avatar:tranformImage(i.creator.avatar,50)
+      }
+    })))
+  }, [])
+
   return (
     <AdminLayout>
-    <div>ChatManagement</div>
+      <Table heading={"all chats"} rows={rows} columns={columns} />
     </AdminLayout>
   )
 }
