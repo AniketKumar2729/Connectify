97d885eb5b2453fd46c128a75da256be034c3b3e
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-10-29 11:07:19 +0530

    Basic server file is created

--- server/app.js
+++ server/app.js
@@ -1,1 +1,9 @@
-console.log("aniket kumar")
+import express from "express";
+const app=express();
+app.get('/',(req,res)=>{
+    res.send("hello user")
+})
+app.listen(3000,()=>{
+    console.log("server is listening");
+    
+})
