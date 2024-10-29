847dd37fbf6078f26a8aa6da9080b64c4887f147
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-10-29 11:28:56 +0530

    User schema(User model) is created with fields

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,7 +1,32 @@
 import { Schema, model, models } from "mongoose";
 
 const userSchema = new Schema({
-
+    name:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        select:false // whenever we will call the user collection every thing will come but password will not come.It will come only when we will forcefully call him.
+    },
+    avatar:{
+        public_id:{
+            type:String,
+            required:true
+        },
+        url:{
+            type:String,
+            required:true
+        }
+    }
+},{
+    timestamps:true
 })
 
 export const User = models.User || model('User', userSchema);
