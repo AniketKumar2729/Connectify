f9f6483df263177cb0e9cf1db57fed70f0b15451
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-11-04 15:49:39 +0530

    Login controller logic is written

--- server/controllers/users.controllers.js
+++ server/controllers/users.controllers.js
@@ -1,3 +1,4 @@
+import { compare } from "bcrypt";
 import { User } from "../models/user.model.js";
 import { sendToken } from "../utils/features.utils.js";
 //create new user and save it to database and return cookie
@@ -14,6 +15,14 @@ export const newUser = async (req, res) => {
     const newUser=await User.create({ name, username, password, bio, avatar })
     sendToken(res,newUser,201,"User Created");
 }
-export const login = (req, res) => {
-    res.send("user is trying to login")
+export const login = async(req, res) => {
+    const {username,password}=req.body;
+    const existedUser=await User.findOne({username}).select("+password"); //.select is used because in our model we separated the password and then send the response due to select() we can get password also
+    const isPasswordMatch= await compare(password,existedUser.password);
+    if(!isPasswordMatch)
+        return res.status(400).json({message:"Invalid password"})
+    else if(!(username===existedUser.username))
+        return res.status(400).json({message:"Invalid username"})
+//    const {password:pass,...rest}=existedUser._doc; //this is how we separate the password from the rest of the field
+    sendToken(res,existedUser,200,`Welcome back,${existedUser.name}`);
 }
