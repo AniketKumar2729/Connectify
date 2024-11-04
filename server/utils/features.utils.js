e63f683cf2b49b439b3d6318e1283258b6eacfb8
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-11-04 15:24:07 +0530

    Random token is created with help of jwt

--- server/utils/features.utils.js
+++ server/utils/features.utils.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose"
-
+import jwt from "jsonwebtoken";
 dotenv.config();
 export const connectDB = (uri) => {
     mongoose.connect(process.env.MONGODB_URI).then(() => {
@@ -9,13 +9,13 @@ export const connectDB = (uri) => {
     )
 }
 const cookieOption={
-    maxAge:15*24*60*60*1000,
+    maxAge:15*24*60*60*1000,//cookie is valid for 15 days
     sameSite:'none',
     httpOnly:true,
     secure:true
 }
 export const sendToken = (res, user, code, message) => {
-    const token = 'jksndfjkasn';
+    const token = jwt.sign({_id:user._id},process.env.JWT_SECRET)
     return res.status(code).cookie('Connectify-token',token,cookieOption).json({
         success: true,
         message,
