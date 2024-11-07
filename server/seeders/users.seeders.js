1d7b79bea0368715f9dda827c7a2acd7e5b9153a
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-11-07 16:05:56 +0530

    fake 10 users are created in database with the help of faker package in seeders file

---
+++ server/seeders/users.seeders.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user.model.js";
+import {faker} from '@faker-js/faker'
+export const createUser=async(numUsers)=>{
+try {
+    const usersPromise=[]
+    for (let i = 0; i < numUsers; i++){
+        const tempUser=User.create({
+            name:faker.person.fullName(),
+            username:faker.internet.userName(),
+            bio:faker.lorem.sentence(10),
+            password:"123456789",
+            avatar:{
+                url:faker.image.avatar(),
+                public_id:faker.system.fileName()
+            }
+        })
+        usersPromise.push(tempUser);
+    }
+    await Promise.all(usersPromise)
+    console.log("user created",numUsers);
+    process.exit(1);
+    
+} catch (error) {
+    console.error(error);
+    process.exit(1)
+    
+}
+}
+createUser(10);
