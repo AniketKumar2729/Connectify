9823e258837d1679f3dc8d4c8edb8c446cc63ccf
Aniket_Kumar29 <aniketgope2729@gmail.com>
2024-12-22 07:47:00 +0530

    Socket is setup for smooth chatting

--- server/app.js
+++ server/app.js
@@ -4,10 +4,65 @@ import cookieParser from "cookie-parser";
 import { chatRouter } from "./routes/chat.routes.js";
 import { userRouter } from "./routes/user.routes.js";
 import { adminRouter } from "./routes/admin.routes.js";
-import { createSampleGroupChat, createSampleMessage, createSampleMessageInAGroup, createSampleSingleChat, createUser } from "./seeders/users.seeders.js";
+import { Server } from 'socket.io'
+import { createServer } from "http"
+import { NEW_MESSAGE } from "./constants/event.constants.js";
+import { v4 as uuid } from "uuid"
+import { getSockets } from "./lib/helper.lib.js";
+import { Message } from "./models/message.model.js";
+
+// import { createSampleGroupChat, createSampleMessage, createSampleMessageInAGroup, createSampleSingleChat, createUser } from "./seeders/users.seeders.js";
 connectDB()
 const app = express();
-app.listen(3000, () => {
+export const userSocketIDs=new Map()
+const server = createServer(app)
+const io = new Server(server, {})
+io.on("connection", (socket) => {
+    const tempUser = {
+        _id: 'abcd',
+        name: "efgh"
+    }
+    userSocketIDs.set(tempUser._id.toString(),socket.id)
+    console.log(userSocketIDs);
+    console.log("An user connected", socket.id)
+    socket.on('NEW_MESSAGE', async ({ chatId, members, message }) => {
+        const membersSockets=getSockets(members)
+        const messageForRealTime = {
+            content: message,
+            _id: uuid(),
+            sender: {
+                _id: tempUser._id,
+                name: tempUser.name
+            },
+            chat: chatId,
+            createdAt: new Date().toISOString()
+        }
+        const messageForDB = {
+            content: message,
+            sender: tempUser._id,
+            chat: chatId
+        }
+        io.to(membersSockets).emit("NEW_MESSAGE",{
+            chatId,
+            message:messageForRealTime
+        }) 
+        io.to(membersSockets).emit("NEW_MESSAGE_ALERT",{
+            chatId
+        })      
+        try {
+            await Message.create9(messageForDB)
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+    })
+    socket.on("disconnect", () => {
+        userSocketIDs.delete(tempUser._id.toString())
+        console.log("User Disconnect");
+    })
+})
+server.listen(3000, () => {
     console.log("server is listening");
 })
 // createSampleSingleChat(10)
@@ -32,14 +87,14 @@ app.use((err, req, res, next) => {
     let statusCode = err.statusCode || 500;
     let message = err.message || "Internal Server Error";
     if (err.code === 11000) {
-        const error=Object.keys(err.keyPattern).join(",")
-        message=`Duplicate field -> ${error}`
-        statusCode=400
+        const error = Object.keys(err.keyPattern).join(",")
+        message = `Duplicate field -> ${error}`
+        statusCode = 400
     }
-    if(err.name==="CastError"){
-        const errorPath=err.path
-        message=`Invalid format of -> ${errorPath}`
-        statusCode=400
+    if (err.name === "CastError") {
+        const errorPath = err.path
+        message = `Invalid format of -> ${errorPath}`
+        statusCode = 400
     }
     res.status(statusCode).json({
         success: false,
--- server/lib/helper.lib.js
+++ server/lib/helper.lib.js
@@ -1,1 +1,8 @@
+import { userSocketIDs } from "../app.js"
+
 export const getOtherMember= (members,userId)=>members.find((member)=>member._id !== userId._id )
+
+export const getSockets=(users=[])=>{
+    const sockets=users.map((user)=>userSocketIDs.get(user._id.toString()))
+    return sockets
+} 
