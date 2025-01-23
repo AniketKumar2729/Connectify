8b1694a20258793952fc5acc72fbf7f0e0df97bb
Aniket_Kumar29 <aniketgope2729@gmail.com>
2025-01-23 22:38:18 +0530

    small bug fixed (previously when user send friend request and other user accept the request the sender will see himself only and fixed the avatar of group chat)

--- client/src/components/shared/AvatarCard.jsx
+++ client/src/components/shared/AvatarCard.jsx
@@ -1,21 +1,38 @@
-import { Avatar, Box, Stack } from '@mui/material'
-import React from 'react'
+import { Avatar, AvatarGroup, Box, Stack } from "@mui/material";
+import React from "react";
+import { tranformImage } from "../../lib/features.js";
 
-const AvatarCard = ({avatar=[],max=4}) => {
+// Todo Transform
+const AvatarCard = ({ avatar = [], max = 4 }) => {
   return (
     <Stack direction={"row"} spacing={0.5}>
-    <Avatar max={max} position={'relative'}>
-    <Box width={'5rem'} height={'3rem'}/>
-    {
-        avatar.map((src,idx)=>(
-            <img key={Math.random()*100} src={src} alt={`Avatar ${avatar}`} style={{widt:'2rem',height:'2rem',position:'absolute',left:{xs:`${0.5 + idx}rem`,sm:`${idx}rem`}}}/>
-        ))
-    }
-
-    </Avatar>
-
+      <AvatarGroup
+        max={max}
+        sx={{
+          position: "relative",
+        }}
+      >
+        <Box width={"5rem"} height={"3rem"}>
+          {avatar.map((i, index) => (
+            <Avatar
+              key={Math.random() * 100}
+              src={tranformImage(i)}
+              alt={`Avatar ${index}`}
+              sx={{
+                width: "3rem",
+                height: "3rem",
+                position: "absolute",
+                left: {
+                  xs: `${0.5 + index}rem`,
+                  sm: `${index}rem`,
+                },
+              }}
+            />
+          ))}
+        </Box>
+      </AvatarGroup>
     </Stack>
-  )
-}
+  );
+};
 
-export default AvatarCard
+export default AvatarCard;
